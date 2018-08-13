a72e38ae04d96da12d65a22c360aa6be51e630d7
Estudiante <Estudiante@10.203.166.40>
2018-08-13 08:38:57 -0500

    commit inicial

---
+++ src/prueba/pkg1/de/java/juano/Prueba1DeJavaJUANO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba.pkg1.de.java.juano;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Prueba1DeJavaJUANO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=2;
+       
+        
+        System.out.println(a+15);
+    }
+    
+}
